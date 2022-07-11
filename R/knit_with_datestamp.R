741897befbad05a538f944a93063b0a6030fd46f
Steven Eschrich <35569991+steveneschrich@users.noreply.github.com>
2022-07-11 09:12:42 -0400

    Added a shiny-based knit option, plus refreshed man pages.

--- R/knit_with_datestamp.R
+++ R/knit_with_datestamp.R
@@ -24,3 +24,33 @@ knit_docx_with_datestamp <- function(input, output_dir = "delivery", ...) {
     envir = globalenv()
   )
 }
+
+
+
+#' Title
+#'
+#' @details This code was taken from
+#'  https://bookdown.org/yihui/rmarkdown-cookbook/custom-knit.html as a way
+#'  to change the output file associated with markdown from within R Studio.
+#'
+#' @param input Input file
+#' @param output_dir Output directory (default: delivery)
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+knit_docx_with_datestamp_shiny <- function(input, output_dir = "delivery", ...) {
+  rmarkdown::render(
+    input,
+    output_dir = here::here(output_dir),
+    output_file = paste0(
+      xfun::sans_ext(input), '_',
+      lubridate::today(),
+      ".docx"
+    ),
+    params = "ask",
+    envir = globalenv()
+  )
+}
